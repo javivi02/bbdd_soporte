17e38fc2d97cfa0c7740ee313501dfdef549cbe2
jga <javivi02@gmail.com>
2023-07-26 14:45:07 +0200

    Afinando componentes y me voy a comer

--- src/hooks/useCkeckHook.js
+++ src/hooks/useCkeckHook.js
@@ -1,16 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { checkJWT } from '../service/check.js'
 
 export const UseCkeckHook = (token) => {
 
   const [bandera, setBandera] = useState(true)
 
-  checkJWT(token)
-    .then((res) => {
-      res === 'Usuario con token correcto'
-        ? setBandera(true)
-        : setBandera(false)
-    })
+  useEffect(() => {
+    checkJWT(token)
+      .then((res) => {
+        res === 'Usuario con token correcto'
+          ? setBandera(true)
+          : setBandera(false)
+      })
+  }, [])
 
   return bandera
 
