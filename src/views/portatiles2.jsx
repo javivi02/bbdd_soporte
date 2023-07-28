a8266d04669eee096007e3ccadbdc906bedd3745
jga <javivi02@gmail.com>
2023-07-28 20:32:22 +0200

    Ultimos cambios

--- src/views/portatiles2.jsx
+++ src/views/portatiles2.jsx
@@ -77,44 +77,47 @@ export const Portatiles2 = ({ props }) => {
 
       </div>
 
-      <ul className="flex items-center -space-x-px h-10 text-base p-4">
-        <li>
-          <button onClick={paginaAnterior}
-                  className="flex items-center justify-center px-4 h-10 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-            <span className="sr-only">Previous</span>
-            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                 viewBox="0 0 6 10">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                    d="M5 1 1 5l4 4"/>
-            </svg>
-          </button>
-        </li>
+      <div className="flex justify-center">
 
-        {
-          numbers.map((e, i) => {
+        <ul className="flex items-center -space-x-px h-10 text-base p-4">
+          <li>
+            <button onClick={paginaAnterior}
+                    className="flex items-center justify-center px-4 h-10 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+              <span className="sr-only">Previous</span>
+              <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                   viewBox="0 0 6 10">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                      d="M5 1 1 5l4 4"/>
+              </svg>
+            </button>
+          </li>
 
-            return (
-              <button key={i}
-                      onClick={() => pagina(e)}
-                      className={`${currentPage === e ? 'flex items-center justify-center px-4 h-10 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white' : 'flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'}`}>
-                {i + 1}
-              </button>
-            )
-          })
-        }
+          {
+            numbers.map((e, i) => {
 
-        <li>
-          <button onClick={paginaSiguiente}
-                  className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-            <span className="sr-only">Next</span>
-            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                 viewBox="0 0 6 10">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                    d="m1 9 4-4-4-4"/>
-            </svg>
-          </button>
-        </li>
-      </ul>
+              return (
+                <button key={i}
+                        onClick={() => pagina(e)}
+                        className={`${currentPage === e ? 'flex items-center justify-center px-4 h-10 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white' : 'flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white'}`}>
+                  {i + 1}
+                </button>
+              )
+            })
+          }
+
+          <li>
+            <button onClick={paginaSiguiente}
+                    className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+              <span className="sr-only">Next</span>
+              <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                   viewBox="0 0 6 10">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                      d="m1 9 4-4-4-4"/>
+              </svg>
+            </button>
+          </li>
+        </ul>
+      </div>
 
       <Tabla portatiles={records}/>
 
